d48528583d4b8b8a529f54313262253bbf0971b4
Juan Camilo <camilo.patino@est.iudigital.edu.co>
2025-05-29 13:46:20 -0500

    Agregando controladores y vistas para servicios

---
+++ src/main/java/com/speedroller/demo/Controller/AlumnosController.java
@@ -0,0 +1,14 @@
+package com.speedroller.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class AlumnosController {
+    @GetMapping("/alumnos")
+    public String index() {
+        return "alumnos";
+    }
+}
